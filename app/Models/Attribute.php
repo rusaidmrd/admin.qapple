40851292168c430ffcd36952616ae772304512ad
Rusaid <rusaidmrd@gmail.com>
2022-04-02 10:15:24 +0530

    Creating the model and migration for Attribute

---
+++ app/Models/Attribute.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(AttributesTableSeeder::class);
     }
 }
