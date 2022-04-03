94de56910537370be0be344f0b71c443b70547e9
Rusaid <rusaidmrd@gmail.com>
2022-04-03 09:04:44 +0530

    adding the model and migration for Product

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -17,4 +17,9 @@ class Brand extends Model
         $this->attributes['name'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
