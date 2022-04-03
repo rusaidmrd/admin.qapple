eb0616118d1d321d384ab803a201cb8ac6b4a05d
Rusaid <rusaidmrd@gmail.com>
2022-04-03 09:10:45 +0530

    Creating the model and migration for ProductImage

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -32,4 +32,9 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
